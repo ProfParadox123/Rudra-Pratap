426455779e689b688ef6ffa76ae837329e4d7441
Chaitanya Mane <chetanmane345@gmail.com>
2025-10-20 18:24:27 +0530

    Chapter 1 done

--- RudraPratap1.m
+++ RudraPratap1.m
@@ -1,2 +1,74 @@
 %Had chickenpox and exams , so continuining after a break
 %Type demo in command window for demo 
+%Types of files : m-files , mat-files , fig-files , p-files , mex-files 
+
+% Genral command to remember 
+
+% On-line help
+%
+% help             % lists topics on which help is available
+% helpwin          % opens the interactive help window
+% helpdesk         % opens the web browser-based help facility
+% help topic       % provides help on topic
+% lookfor string   % lists help topics containing string
+% demo             % runs the demo program
+
+% Workspace information
+%
+% who           % lists variables currently in the workspace
+% whos          % lists variables currently in the workspace with their size
+% what          % lists all .m, .mat, and .m-files on the disk
+% clear         % clears the workspace, all variables are removed
+% clear x y z   % clears only variables x, y, and z
+% clear all     % clears all variables and functions from workspace
+% mlock fun     % locks function fun so that clear cannot remove it
+% munlock fun   % unlocks function fun so that clear can remove it
+% clc           % clears command window, cursor moves to the top
+% home          % scrolls the command window to put the cursor on top
+% clf           % clears figure window
+
+
+% Directory information
+%
+% pwd           % shows the current working directory
+% cd            % changes the current working directory
+% dir           % lists contents of the current directory
+% ls            % lists contents of the current directory, same as dir
+% path          % sets or gets MATLAB search path
+% editpath      % modifies MATLAB search path
+% copyfile      % copies a file
+% mkdir         % creates a directory
+
+% File operations
+%
+% fopen         % opens a file and returns a file identifier
+% fclose        % closes an open file
+% fread         % reads binary data from a file
+% fwrite        % writes binary data to a file
+% fprintf       % writes formatted data to a text file
+% fscanf        % reads formatted data from a text file
+
+
+% General information
+%
+% computer      % tells you the computer type you are using
+% clock         % gives you wall clock time and date as a vector
+% date          % tells you the date as a string
+% more          % controls the paged output according to the screen size
+% ver           % gives the license and the MATLAB version information
+% bench         % benchmarks your computer on running MATLAB compared to other computers
+
+% Additional commands
+%
+% save filename   % saves workspace variables to a .mat file
+% load filename   % loads variables from a .mat file
+% plot(x, y)     % creates a 2D plot of y versus x
+% figure         % creates a new figure window
+
+
+% Termination
+%
+% ^c (Control-c)  % local abort, kills the current command execution
+% quit            % quits MATLAB
+% exit            % same as quit
+  
